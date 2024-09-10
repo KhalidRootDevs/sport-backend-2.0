23c98225fa3912d5ae64eef2341b9cc3c9c64d0d
Prantadas <prantodas043@gmail.com>
2024-09-10 15:12:13 +0600

    update: data cached for fixtures

--- src/features/sportsMonk/routes.ts
+++ src/features/sportsMonk/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { authenticate } from '../../middlewares/authenticate';
 import { getFixtureMonks, searchLeagues } from './controller';
+import { redisCache } from '../../services/redis';
 
 const router = Router();
 
 // Get formatted fixture
-router.post('/fixtures/formatted', getFixtureMonks);
+router.post('/fixtures/formatted', redisCache.cachingMiddleware(), getFixtureMonks);
 
 router.get('/leagues/search', searchLeagues);
 
